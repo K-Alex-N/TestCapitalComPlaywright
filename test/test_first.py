a48cfeec26cdeb5daaa13f76ad495c28628c7c72
Alexey Kurochkin <KurochkinAlexei@yandex.ru>
2024-12-13 10:18:22 +0100

    initial

--- test/test_first.py
+++ test/test_first.py
@@ -1,21 +1,10 @@
-# from pages.login_page import LoginPage
 from pages.login_page import LoginPage
 
-from playwright.sync_api import sync_playwright
 
+def test_login(browser_context):
+    page = browser_context.new_page()
+    login_page = LoginPage(page)
+    login_page.navigate_to("https://www.saucedemo.com/")
+    login_page.login("standard_user", "secret_sauce")
 
-def test_login():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        # Создаём новый контекст браузера (это изолированная сессия)
-        context = browser.new_context()
-        # Открываем новую вкладку
-        page = context.new_page()
-        login_page = LoginPage(page)
-        login_page.navigate_to("https://www.saucedemo.com/")
-        login_page.login("standard_user", "secret_sauce")
-
-        assert login_page.is_login_successful(), "Не удалось авторизоваться!"
-
-        context.close()
-        browser.close()
+    assert login_page.is_login_successful(), "Не удалось авторизоваться!"
